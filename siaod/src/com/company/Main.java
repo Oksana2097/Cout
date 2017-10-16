bae01a80462406fa510f816ae8bb31d83b3b9c98
оксана <ijdgkfdjkve@.ru>
2017-10-16 11:04:51 +0700

    coming

---
+++ siaod/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA=) ^^");
+    }
+}
